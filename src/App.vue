afcf3fc8ec81c08f6347a6303272bc0635fcf700
Bruno Furtado <brunofurtadoc@gmail.com>
2018-11-03 23:16:14 +0100

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,15 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <div id="urls">
+      <h1>Pages</h1>
+      <ul>
+        <li><router-link to="/">/</router-link></li>
+        <li><router-link to="/foo">/foo</router-link></li>
+        <li><router-link to="/bar">/bar</router-link></li>
+      </ul>
+      <router-view class="view"></router-view>
+    </div>
   </div>
 </template>
 
