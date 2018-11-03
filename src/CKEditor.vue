b0ec034a3a27bb66c55820180148f0340b05cf31
Bruno Furtado <brunofurtadoc@gmail.com>
2018-11-04 00:19:52 +0100

    Add CKEditor5

--- src/CKEditor.vue
+++ src/CKEditor.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>{{msg}}</div>
+  <div>
+    <h1>{{ msg }}</h1>
+  	<ckeditor
+  		editor="classic"
+  		tag-name="textarea"
+  		v-model="editorData"
+  		:config="editorConfig"
+  		:disabled="editorDisabled"
+  		@ready="onEditorReady"
+  		@focus="onEditorFocus"
+  		@blur="onEditorBlur"
+  		@input="onEditorInput"
+  		@destroy="onEditorDestroy"
+  		></ckeditor>
+  	<button v-on:click="toggleEditorDisabled()">
+  		{{ editorDisabled ? 'Enable' : 'Disable' }} editor
+  	</button>
+
+  	<h2>Live editor data</h2>
+  	<code>{{ editorData }}</code>
+  </div>
 </template>
 <script>
 export default {
   name: 'CKEditor',
   data () {
     return {
-      msg: 'Welcome to CKEditor 5!'
+      msg: 'Welcome to CKEditor 5!',
+      editorData: '<p>Hello world!</p>',
+			editorConfig: { toolbar: [ 'heading', '|', 'bold', 'italic' ] },
+			editorDisabled: false
     }
-  }
+  },
+  methods: {
+		toggleEditorDisabled() {
+			this.editorDisabled = !this.editorDisabled;
+		},
+    onEditorReady( editor ) {
+		  console.log( 'Editor is ready.', { editor } );
+	  },
+	  onEditorFocus( event, editor ) {
+		  console.log( 'Editor focused.', { event, editor } );
+	  },
+	  onEditorBlur( event, editor ) {
+		  console.log( 'Editor blurred.', { event, editor } );
+	  },
+	  onEditorInput( data, event, editor ) {
+		  console.log( 'Editor data input.', { event, editor, data } );
+	  },
+	  onEditorDestroy( editor ) {
+		  console.log( 'Editor destroyed.', { editor } );
+	  },
+  },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import BootstrapVue from "bootstrap-vue"
+import BootstrapVue from 'bootstrap-vue'
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 import App from './App.vue'
 import AppRouter from './router.js'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
+
+Vue.use( CKEditor, {
+  editors: {
+    classic: ClassicEditor
+  },
+});
 
 new Vue({
   el: '#app',
